e4ed90bbe6f98271e98973a4f8a2a3761540f0d3
ZZELAV <valentino.panico@icloud.com>
2023-05-13 22:28:11 +0200

    text korrektur

--- vue-app/src/components/TextShortComponent.vue
+++ vue-app/src/components/TextShortComponent.vue
@@ -1,8 +1,7 @@
 <template>
   <p>
-    Ich bin Valentino Panico und bin am 28.08.2004 geboren. Ich wohne in
-    Kreuzlingen und mache eine Lehre als Informatiker bei der Lenze Swiss AG in
-    Romanshorn.
+    Ich bin Valentino Panico und am 28.08.2004 geboren. Ich wohne in Kreuzlingen
+    und mache eine Lehre als Informatiker bei der Lenze Swiss AG in Romanshorn.
   </p>
 </template>
 
