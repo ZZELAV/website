7f333b40395010e1875d30fcceb4845771f8e39b
ZZELAV <valentino.panico@icloud.com>
2023-05-13 18:52:12 +0200

    deploy

--- vue-app/deploy.bat
+++ vue-app/deploy.bat
@@ -1,9 +1,8 @@
 @echo off
 
 npm "run" "build"
-cd "dist"
-git "init"
+cd ".\dist\"
 git "add" "-A"
 git "commit" "-m" "deploy"
-git "push" "-f" "git@github.com:zzelav\website.git" "main:gh-pages"
+git "push" "-f" "git@github.com:zzelav/website.git" "main:gh-pages"
 cd "-"
