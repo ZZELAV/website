87e3219e4d850ac6940fe206cf892005dde60d50
ZZELAV <valentino.panico@icloud.com>
2023-05-15 11:34:02 +0200

    projects template

---
+++ vue-app/src/components/ProjectsComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <main>
+    <div class="projects__card-container">
+      <section class="projects__card" v-for="item in items">
+        <img :src="item.img" :alt="item.title" />
+        <div class="projects__card-text-container">
+          <p class="projects__card-title">{{ item.title }}</p>
+          <p class="projects__card-description">{{ item.description }}</p>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          img: "https://picsum.photos/300/200",
+          title: "Card-1",
+          description: "Description-1",
+        },
+        {
+          img: "https://picsum.photos/300/200",
+          title: "Card-2",
+          description: "Description-2",
+        },
+        {
+          img: "https://picsum.photos/300/200",
+          title: "Card-3",
+          description: "Description-3",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "@/assets/main.css";
+
+.projects__card-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.projects__card {
+  margin: var(--rem-300);
+  border: var(--rem-100) solid var(--color-500);
+  border-radius: 10px;
+  background-color: var(--color-500);
+}
+.projects__card img {
+  padding: var(--rem-200);
+}
+.projects__card-text-container {
+  background-color: var(--color-600);
+  border-radius: 0 0 10px 10px;
+  padding: var(--rem-200);
+}
+.projects__card-title {
+  color: var(--color-200);
+  font-weight: bold;
+}
+.projects__card-description {
+  font-size: 0.8rem;
+}
+</style>
--- vue-app/src/views/ProjectsView.vue
+++ vue-app/src/views/ProjectsView.vue
@@ -1,7 +1,9 @@
 <template>
   <HeaderComponent />
+  <ProjectsComponent />
 </template>
 
 <script setup>
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import ProjectsComponent from "@/components/ProjectsComponent.vue";
 </script>
